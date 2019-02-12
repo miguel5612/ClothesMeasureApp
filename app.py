18e3706b4f76713631825dad72bd8a343468b295
miguel <miguelangelcu@ufps.edu.co>
2019-02-12 14:42:45 -0500

    Fixed first app 5

--- app.py
+++ app.py
@@ -48,5 +48,4 @@ def lol():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, use_reloader=True)
