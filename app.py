e00d628e15c166e2a2913793f110f3cf6ef01b2c
miguel <miguelangelcu@ufps.edu.co>
2019-02-12 09:42:34 -0500

    Basic app uploaded in python

--- app.py
+++ app.py
@@ -3,19 +3,30 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 from sklearn.externals import joblib
 
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import argparse
+import imutils
+import cv2
+
 #   from settings import PROJECT_ROOT
-#import os
+import os
 
 app = Flask(__name__)
 
 #PEOPLE_FOLDER = os.path.join('static', 'people_photo')
 #app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
-model = joblib.load('decisiontreeiris.pkl')
+# model = joblib.load('decisiontreeiris.pkl')
 
-app.config["CACHE_TYPE"] = "null"
+# app.config["CACHE_TYPE"] = "null"
 # some time later
-cache.init_app(app)
+#cache.init_app(app)
 
 @app.route('/', methods=['GET'])
 def index(name=None):
@@ -24,21 +35,16 @@ def index(name=None):
 def lol():
 	return "lol"
 
-@app.route('/API/iris', methods=['POST'])
-def ApiIris():
+#@app.route('/API/iris', methods=['POST'])
+#def ApiIris():
+#    datos = []    
+#    datos.append(float(request.form['sepal_length']))
+#    datos.append(float(request.form['sepal_width']))
+#    datos.append(float(request.form['petal_length']))
+#    datos.append(float(request.form['petal_width']))
 
-	try:
+#    return jsonify(model.predict([datos])[0])
 
-    datos = []    
-    datos.append(float(request.form['sepal_length']))
-    datos.append(float(request.form['sepal_width']))
-    datos.append(float(request.form['petal_length']))
-    datos.append(float(request.form['petal_width']))
-
-    return jsonify(model.predict([datos])[0])
-
-    catch(err):
-    	return "Error en el request " + err
 
 
 if __name__ == '__main__':
